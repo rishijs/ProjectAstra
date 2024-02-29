c6b58bbdea57d180f91581fbf678765fe08e675c
rj02sd <98123662+rj02sd@users.noreply.github.com>
2024-02-29 14:31:27 -0500

    setup with data and globals

--- data/process_data.gd
+++ data/process_data.gd
@@ -4,16 +4,23 @@ var all_data = {}
 
 #definer, path
 @export var csv_files:Dictionary = {
-	"test":"res://data/csv/test.csvdata"
+	"weapons":"res://data/csv/weapons.csvdata",
+	"aberrations":"res://data/csv/aberrations.csvdata",
+	"enemies":"res://data/csv/enemies.csvdata"
 }
+var weapon_identifier = "weapons"
+enum weapon_attributes {NAME,CHROMA,ARCHETYPE,DAMAGE,MAGAZINE,
+FIRE_RATE,CRITICAL_CHANCE,CRITICAL_DAMAGE,DAMAGE_ID,ABERRATION_OBJECTIVE,
+ABERRATION_TIME,ABERRATION_MOVEMENT}
+var aberration_identifier = "aberrations"
+enum aberration_attributes {NAME,FLAT_BONUS,MULTIPLIED_BONUS,ID,DESCRIPTION}
+var enemy_identifier = "enemies"
+enum enemy_attributes {NAME,HEALTH,DAMAGE,ID}
 
 func _ready():
 	for type in csv_files.keys():
 		all_data[type] = csv_to_dict(csv_files[type])
-	#print(all_data)
-	#print("test access attribute: ",get_attribute(csv_files.keys()[0],"test","number"))
-	#print("test access item: ",get_item(csv_files.keys()[0],"test"))
-	#print("test access type: ",get_type(csv_files.keys()[0]))
+	#print("default weapon fire rate: %f [test access]"%[get_weapon_attribute("default",weapon_attributes.FIRE_RATE)])
 
 #takes in a csv file path and returns a dictionary of values it reads
 #assumes you have headers in the csv along with the first column of each row being names
@@ -43,17 +50,80 @@ func csv_to_dict(filepath,delimiter = ','):
 
 #easier way to get data without worrying about retyping the same line over and over again
 #manually defined name, row name, col name
-func get_attribute(type,item,attribute):
-	if (type in all_data) and (item in all_data[type]) and (attribute in all_data[type][item]):
-		return all_data[type][item][attribute]
+func get_weapon_attribute(item,attribute:weapon_attributes):
+	if item is int:
+		if len(all_data[weapon_identifier].keys()) > item:
+			item = all_data[weapon_identifier].keys()[item]
+	if (weapon_identifier in all_data) and (item in all_data[weapon_identifier]):
+		match attribute:
+			weapon_attributes.NAME:
+				return all_data[weapon_identifier][item]["weapon"]
+			weapon_attributes.CHROMA:
+				return all_data[weapon_identifier][item]["chroma"]
+			weapon_attributes.ARCHETYPE:
+				return all_data[weapon_identifier][item]["archetype"]
+			weapon_attributes.DAMAGE:
+				return all_data[weapon_identifier][item]["damage"]
+			weapon_attributes.MAGAZINE:
+				return all_data[weapon_identifier][item]["magazine"]
+			weapon_attributes.FIRE_RATE:
+				return all_data[weapon_identifier][item]["fire rate"]
+			weapon_attributes.CRITICAL_CHANCE:
+				return all_data[weapon_identifier][item]["critical chance"]
+			weapon_attributes.CRITICAL_DAMAGE:
+				return all_data[weapon_identifier][item]["critical damage"]
+			weapon_attributes.DAMAGE_ID:
+				return all_data[weapon_identifier][item]["damage id"]
+			weapon_attributes.ABERRATION_OBJECTIVE:
+				return all_data[weapon_identifier][item]["aberration objective"]
+			weapon_attributes.ABERRATION_TIME:
+				return all_data[weapon_identifier][item]["aberration time"]
+			weapon_attributes.ABERRATION_MOVEMENT:
+				return all_data[weapon_identifier][item]["aberration movement"]
+			_:
+				printerr("weapon attribute not found")
+	else:
+		printerr("weapon data not found")
 
-func get_item(type,item):
-	if (type in all_data) and (item in all_data[type]):
-		return all_data[type][item]
-	
-func get_type(type):
-	if (type in all_data):
-		return all_data[type]
+func get_aberration_attribute(item,attribute:aberration_attributes):
+	if item is int:
+		if len(all_data[aberration_identifier].keys()) > item:
+			item = all_data[aberration_identifier].keys()[item]
+	if (aberration_identifier in all_data) and (item in all_data[aberration_identifier]):
+		match attribute:
+			aberration_attributes.NAME:
+				return all_data[aberration_identifier][item]["aberration"]
+			aberration_attributes.FLAT_BONUS:
+				return all_data[aberration_identifier][item]["flat bonus"]
+			aberration_attributes.MULTIPLIED_BONUS:
+				return all_data[aberration_identifier][item]["multiplied bonus"]
+			aberration_attributes.ID:
+				return all_data[aberration_identifier][item]["id"]
+			aberration_attributes.DESCRIPTION:
+				return all_data[aberration_identifier][item]["description"]
+			_:
+				printerr("aberration attribute not found")
+	else:
+		printerr("aberration data not found")
+		
+func get_enemy_attribute(item,attribute:enemy_attributes):
+	if item is int:
+		if len(all_data[enemy_identifier].keys()) > item:
+			item = all_data[enemy_identifier].keys()[item]
+	if (enemy_identifier in all_data) and (item in all_data[enemy_identifier]):
+		match attribute:
+			enemy_attributes.NAME:
+				return all_data[enemy_identifier][item]["enemy"]
+			enemy_attributes.HEALTH:
+				return all_data[enemy_identifier][item]["health"]
+			enemy_attributes.DAMAGE:
+				return all_data[enemy_identifier][item]["damage"]
+			enemy_attributes.ID:
+				return all_data[enemy_identifier][item]["id"]
+			_:
+				printerr("enemy attribute not found")
+	else:
+		printerr("enemy data not found")
 		
 func _process(_delta):
 	pass
--- scenes/globals.gd
+++ scenes/globals.gd
@@ -1,11 +1,40 @@
 extends Node
 
+var player_preferences = {
+	"mouse sensitivity" = 0.01
+}
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var in_game = false
+var is_paused = false
 
+var time_elapsed = 0
+var time_string = "00:00"
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _input(event):
+	if Input.is_action_just_pressed("pause") and in_game:
+		if get_tree().paused:
+			get_tree().paused = false
+		else:
+			get_tree().paused = true
+
+func convert_time_to_string(mins,seconds):
+	var result:String
+	if seconds < 10 and mins < 10:
+		result = "0%d:0%d"%[mins,seconds]
+	elif seconds < 10 and mins >= 10:
+		result = "%d:0%d"%[mins,seconds]
+	elif seconds >= 10 and mins < 10:
+		result = "0%d:%d"%[mins,seconds]
+	else:
+		result = "%d:%d"%[mins,seconds]
+	return result
+
+func tick_time():
+	time_elapsed += 1
+	var mins = time_elapsed / 60
+	var seconds = time_elapsed % 60
+	time_string = convert_time_to_string(mins,seconds)
+	
+func _on_timer_timeout():
+	if in_game:
+		tick_time()
